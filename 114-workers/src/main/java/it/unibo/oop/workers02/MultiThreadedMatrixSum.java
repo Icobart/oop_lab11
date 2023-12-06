57e8b673eeec7eccde3f6b2e744a47d3e79998bb
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2023-12-06 18:10:45 +0100

    setup for the implementation of the Workers and the method sum

--- 114-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedMatrixSum.java
+++ 114-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedMatrixSum.java
@@ -1,10 +1,13 @@
 package it.unibo.oop.workers02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Implementation of a matrix sum.
  * 
  */
-public final class MultiThreadedSumMatrix implements SumMatrix {
+public final class MultiThreadedMatrixSum implements SumMatrix {
 
     private final int nthread;
 
@@ -12,18 +15,72 @@ public final class MultiThreadedSumMatrix implements SumMatrix {
      * 
      * @param nthread no. of thread performing the sum.
      */
-    public MultiThreadedSumMatrix(final int nthread) {
+    public MultiThreadedMatrixSum(final int nthread) {
         this.nthread = nthread;
     }
 
     private static class Worker extends Thread {
+        private final double[][] matrix;
+        private final int startpos;
+        private final int nelem;
+        private long res;
 
+        /**
+         * Build a new worker.
+         * 
+         * @param matrix
+         *            the matrix to sum
+         * @param startpos
+         *            the initial position for this worker
+         * @param nelem
+         *            the no. of elems to sum up for this worker
+         */
+        Worker(final double[][] matrix, final int startpos, final int nelem) {
+            super();
+            this.matrix = matrix;
+            this.startpos = startpos;
+            this.nelem = nelem;
+        }
+
+        @Override
+        @SuppressWarnings("PMD.SystemPrintln")
+        public void run() {
+            System.out.println("Working from position " + startpos + " to position " + (startpos + nelem - 1));
+            for (int i = startpos; i < matrix.length && i < startpos + nelem; i++) {
+                this.res += this.matrix[i][i]; //does not work, only as placeholder
+            }
+        }
+
+        /**
+         * Returns the result of summing up the integers within the list.
+         * 
+         * @return the sum of every element in the matrix
+         */
+        public long getResult() {
+            return this.res;
+        }
     }
 
     @Override
-    public double sum(double[][] matrix) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sum'");
+    public double sum(final double[][] matrix) {
+        final int size = matrix.length % nthread + matrix.length / nthread;
+        final List<Worker> workers = new ArrayList<>(nthread);
+        for (int start = 0; start < matrix.length; start += size) {
+            workers.add(new Worker(matrix, start, size));
+        }
+        for (final Worker w: workers) {
+            w.start();
+        }
+        long sum = 0;
+        for (final Worker w: workers) {
+            try {
+                w.join();
+                sum += w.getResult();
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+        return sum;
     }
 
 }
